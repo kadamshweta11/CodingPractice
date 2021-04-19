04a9fea746d00cc307bc2d48fabd6b4fc22859c8
kadamshweta11 <67953305+kadamshweta11@users.noreply.github.com>
2021-04-19 11:23:46 +0530

    Update Pattern8.java

--- Day8/Pattern8.java
+++ Day8/Pattern8.java
@@ -2,6 +2,13 @@ package Day8;
 
 import java.util.Scanner;
 
+//6
+* * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
 public class Pattern8 {
 
 	public static void main(String[] args) {
@@ -9,26 +16,18 @@ public class Pattern8 {
 	int n=sc.nextInt();
 	for(int i=0;i<n;i++)
 	{
-//		for(int j=1;j<=i;j++)
-//		{
-//			System.out.print(" ");
-//		}
-//		for(int j=1;j<=n-i+1;j++)
-//		{
-//			System.out.print("* ");
-//		}
-//		System.out.println();
-		
-		
-		for(int j=0;j<2*i;j++)
+		for(int j=1;j<=i;j++)
 		{
 			System.out.print(" ");
 		}
-		for(int j=1;j<n-i+1;j++)
+		for(int j=1;j<=n-i+1;j++)
 		{
 			System.out.print("* ");
 		}
 		System.out.println();
+		
+		
+		
 	}
 
 	}
