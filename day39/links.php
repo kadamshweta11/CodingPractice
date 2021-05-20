5e217f001b853609ee7a5a2cfa18bf39d8ed70cb
kadamshweta11 <67953305+kadamshweta11@users.noreply.github.com>
2021-05-20 21:04:44 +0530

    Add files via upload

---
+++ day39/links.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<h3>Registration  Successful</h3>
+</head>
+<body>
+values you have entered are:<br>
+<?php
+echo "Name=", $_POST["Username"];
+?><br>
+
+<?php
+echo "Email=",$_POST["email"];
+?><br>
+
+<?php
+echo "Contact=",$_POST["Number"];
+?><br>
+
+<?php
+echo "password=",$_POST["password"];
+?><br>
+
+<?php
+echo "password=",$_POST["password"];
+?><br>
+</body>
+</html>
