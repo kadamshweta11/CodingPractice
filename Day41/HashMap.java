5ad1922ee640d6a5da050965fea5bec3e7e5c49f
kadamshweta11 <67953305+kadamshweta11@users.noreply.github.com>
2021-05-22 15:47:51 +0530

    Create HashMap.java

---
+++ Day41/HashMap.java
@@ -0,0 +1,15 @@
+//simple hashmap prog
+
+import java.util.*;
+public class hashmap{
+public static void main(String.args[]){
+HashMap<Integer,String> map=new HashMap<Integer,String> () ;
+map.put(1,"vighnesh");
+map.put(2,"mithilesh");
+map.put(3,"sahil");
+map.put(4,"shweta");
+for(Map. Entry m:map.entrySet()){
+System.out.println(m.getKey() +"" +m.getValue() );
+}
+}
+}
