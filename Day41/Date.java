755b43c39fd1a100162998fa7745b1f01c36972f
kadamshweta11 <67953305+kadamshweta11@users.noreply.github.com>
2021-05-22 15:25:04 +0530

    Create Date.java

---
+++ Day41/Date.java
@@ -0,0 +1,9 @@
+//write a java code to print the current date 
+
+import java.util.*;
+public class Date{
+public static void main(String.args[]) {
+Date d=new Date();
+System.out.println("today's date is :" + d);
+}
+} 
