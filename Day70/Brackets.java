431c117bf889600b42dd151eead7ac0e41b55ad6
kadamshweta11 <67953305+kadamshweta11@users.noreply.github.com>
2021-06-21 09:21:41 +0530

    Add files via upload

---
+++ Day70/Brackets.java
@@ -0,0 +1,35 @@
+package Day70;
+
+import java.util.Stack;
+
+public class Brackets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="({}[]";
+		Stack<Character> st=new Stack<>();
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)=='(' || str.charAt(i)=='{' || str.charAt(i)=='[') {
+				st.push(str.charAt(i));
+			}else if(!st.empty() &&(
+					(str.charAt(i)==')' && st.peek()=='(') ||
+					(str.charAt(i)=='}' && st.peek()=='{') ||
+					(str.charAt(i)==']' && st.peek()=='[')
+					)){
+				st.pop();
+				
+			}
+			else {
+				st.push(str.charAt(i));
+			}
+		}
+		if(st.empty()) {
+			System.out.println("Balanced");
+			
+		}
+		else {
+			System.out.println("Not Balanced");
+		}
+	}
+
+}
